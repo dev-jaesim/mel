edc58b64df7e36a341701c8bc9a9aa539216823a
dev-jaesim <dev.jaesim@gmail.com>
2020-06-10 09:49:26 +1000

    initial commit

--- server/server.js
+++ server/server.js
@@ -31,19 +31,10 @@ app.use("/uploads", express.static("uploads"));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-
-  // index.html for all page routes
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  // });
 }
 
 // ==================== for heroku local ==================== //
 // app.use(express.static("client/build"));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-// });
 
 app.use("/api/users", require("./routes/userRoute"));
 
