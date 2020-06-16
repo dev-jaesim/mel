5546c092ecc04fe4252e7dbb4af95e8b443e8a96
dev-jaesim <dev.jaesim@gmail.com>
2020-06-16 19:11:08 +1000

    TEST: Deploy test

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ app.use("/uploads", express.static("uploads"));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 // ==================== for heroku local ==================== //
